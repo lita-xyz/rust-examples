01a4dfc32b7ff2b84b98c911b25008768d761d7d
thealmarty <thealmartyblog@gmail.com>
2024-11-04 11:01:40 -0800

    Add factoria.

---
+++ factorial/src/main.rs
@@ -0,0 +1,26 @@
+#![no_main]
+
+valida_rs::entrypoint!(main);
+
+pub fn main() {
+    valida_rs::io::println("Please enter a number from 0 to 12:");
+    let num = loop {
+        match valida_rs::io::read_line::<u32>() {
+            Ok(num) => {
+                if num > 12 {
+                    valida_rs::io::println("Number must be between 0 and 12. Please try again:");
+                    continue;
+                }
+                break num
+            },
+            Err(e) => {
+                valida_rs::io::println(&format!("Error reading input: {}. Please try again:", e));
+            }
+        }
+    };
+    let mut factorial:u32 = 1;
+    for i in 1..=num {
+        factorial *= i;
+    }
+    valida_rs::io::println(&format!("Factorial of {} is {}", num, factorial));
+}
