493d04b6006e431124f66bc8d32db6f717bf2d45
thealmarty <thealmartyblog@gmail.com>
2024-11-04 10:36:21 -0800

    Add palindrome.

---
+++ palindrome/src/main.rs
@@ -0,0 +1,17 @@
+#![no_main]
+
+valida_rs::entrypoint!(main);
+
+pub fn main() {
+    valida_rs::io::println("Please enter a word or phrase:");
+    let word = match valida_rs::io::read_line::<String>() {
+        Ok(w) => w,
+        Err(e) => {
+            valida_rs::io::println(&format!("Error reading input: {}", e));
+            return;
+        }
+    };
+    let word_no_spaces = word.chars().filter(|c| !c.is_whitespace()).collect::<String>();
+    let is_palindrome = word_no_spaces.chars().eq(word_no_spaces.chars().rev());
+    valida_rs::io::println(&format!("Is '{}' a palindrome? {}", word, is_palindrome));
+}
